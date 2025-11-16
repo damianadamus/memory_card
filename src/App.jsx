b3a42a20fd9fc9943b47288bce62bce951ff19b1
Damian Adamus <adamus.damian@yahoo.com>
2025-11-16 20:15:23 +0100

    Refactoring code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { GameHeader } from "./components/GameHeader";
 import { Card } from "./components/Card";
-import { useEffect, useState } from "react";
 import { WinMessage } from "./components/WinMessage";
+import { useGameLogic } from "./hooks/useGameLogic";
 
 const cardValues = [
   "🍎",
@@ -23,110 +23,14 @@ const cardValues = [
 ];
 
 function App() {
-  const [cards, setCards] = useState([]);
-  const [flippedCards, setFlippedCards] = useState([]);
-  const [matchedCards, setMatchedCards] = useState([]);
-  const [moves, setMoves] = useState(0);
-  const [score, setScore] = useState(0);
-  const [isLocked, setIsLocked = useState(false);
-
-  const shuffleArray = (array) => {
-    const shuffled = [...array];
-    for (let i = shuffled.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-    }
-    return shuffled;
-  };  
-
-  const initializeGame = () => {
-    // Shuffle cards
-
-    const shuffledValues = shuffleArray(cardValues);
-
-    const finalCards = cardValues.map((value, index) => ({
-      id: index,
-      value,
-      isFlipped: false,
-      isMatched: false,
-    }));
-    setCards(finalCards);
-    setIsLocked(false);
-    setMoves(0);
-    setScore(0);
-    setFlippedCards([]);
-    setMatchedCards([]);
-  };
-
-  useEffect(() => {
-    initializeGame();
-  }, []);
-
-  const handleCardClick = (card) => {
-    // Don't allow checking if card is already flipped, matched
-    if (
-      card.isFlipped || 
-      card.isMatched || 
-      isLocked || 
-      flippedCards.length === 2)
-      { 
-        return
-      };
-
-    // Update card flipped state
-    const newCards = cards.map((c) => {
-      if (c.id === card.id) {
-        return { ...c, isFlipped: true };
-      } else {
-        return c;
-      }
-    });
-    setCards(newCards);
-
-    const newFlippedCards = [...flippedCards, card.id];
-    setFlippedCards(newFlippedCards);
-
-    // Check for match if two cards are flipped
-    if (flippedCards.length === 1) {
-      setIsLocked(true);
-      const firstCard = cards[flippedCards[0]];
-
-      if (firstCard.value === card.value) {
-        setTimeout(() => {
-          setMatchedCards((prev) => [...prev, firstCard.id, card.id]);
-          setScore((prev) => prev + 1);
-          setCards((prev) =>
-            prev.map((c) => {
-              if (c.id === card.id || c.id === firstCard.id) {
-                return { ...c, isMatched: true };
-              } else {
-                return c;
-              }
-            })
-          );
-          setFlippedCards([]);
-          setIsLocked(true);
-        }, 500);
-      } else {
-        // Flip back card 1, card 2
-        setTimeout(() => {
-          const flippedBackCard = newCards.map((c) => {
-            if (newFlippedCards.includes(c.id) || c.id === card.id) {
-              return { ...c, isFlipped: false };
-            } else {
-              return c;
-            }
-          });
-          setCards(flippedBackCard);
-          setIsLocked(true);
-          setFlippedCards([]);
-        }, 1000);
-      }
-      setMoves((prev) => prev + 1);
-    }
-  };
-
-  const isGameComplete = matchedCards.length === cardValues.length;
+  const {
+    cards,
+    moves,
+    score,
+    isGameComplete,
+    handleCardClick,
+    initializeGame,
+  } = useGameLogic(cardValues);
 
   return (
     <div className="app">
@@ -134,7 +38,11 @@ function App() {
       {isGameComplete && <WinMessage score={score} moves={moves} />}
       <div className="cards-grid">
         {cards.map((card) => (
-          <Card card={card} onClick={handleCardClick} />
+          <Card
+            card={card}
+            onClick={handleCardClick}
+            key={Math.random * 1234}
+          />
         ))}
       </div>
     </div>
